0bf02dfcdd289a587dba8b939427513939d970d5
ryancrager <ryancrager@gmail.com>
2026-07-07 13:25:04 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+===========================================================
+BOTW Digital Program
+Version 0.1
+-----------------------------------------------------------
+United States Air Force Band of the West
+===========================================================
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("BOTW Digital Program Loaded");
+
+});
